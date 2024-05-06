1d5198d09d5f7701eec231226672a788d6fd7cde
manwax <102606433+RegameyManuel@users.noreply.github.com>
2024-05-06 15:52:43 +0200

    Update install_ubuntu_24-04.sh
    Maj Mariadb

--- install_ubuntu_24-04.sh
+++ install_ubuntu_24-04.sh
@@ -5,67 +5,62 @@ read -p "Entrez votre nom pour Git: " name
 read -p "Entrez votre adresse email pour Git: " email
 
 # Mise à jour des paquets et installation des dépendances nécessaires
-sudo apt update
+sudo apt update && sudo apt upgrade -y
 sudo apt install curl software-properties-common wget gpg apt-transport-https ca-certificates lsb-release git -y
 
 # Installation d'Apache2
 sudo apt install apache2 -y
-# Activer et démarrer le service Apache2
 sudo systemctl enable apache2
 sudo systemctl start apache2
 
-# Configurer le nom et l'email pour Git
+# Installation de PHP 8.3 et des extensions nécessaires
+sudo apt install php8.3 php8.3-mysql php8.3-curl php8.3-mbstring php8.3-xml php8.3-intl php8.3-gd php8.3-zip -y
+sudo a2enmod php8.3
+sudo systemctl restart apache2
+
+# Configuration du git global
 git config --global user.name "$name"
 git config --global user.email "$email"
 
-# Installation de PHP 8.3 et extensions
-sudo apt install php8.3 php8.3-mysql php8.3-curl php8.3-mbstring php8.3-xml php8.3-intl -y
-# Configuration d'Apache pour utiliser PHP
-sudo a2enmod php8.3
-
-# Installation des applications
-# Google Chrome
-wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | gpg --dearmor | sudo tee /usr/share/keyrings/google-archive-keyring.gpg >/dev/null
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/google-archive-keyring.gpg] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google.list
-
-# Microsoft Edge
-wget -q -O - https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /usr/share/keyrings/microsoft-archive-keyring.gpg >/dev/null
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/microsoft-archive-keyring.gpg] https://packages.microsoft.com/repos/edge stable main" | sudo tee /etc/apt/sources.list.d/microsoft-edge-dev.list
-
-# VS Code
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /usr/share/keyrings/vscode-archive-keyring.gpg >/dev/null
-echo "deb [arch=amd64,arm64,armhf signed-by=/usr/share/keyrings/vscode-archive-keyring.gpg] https://packages.microsoft.com/repos/code stable main" | sudo tee /etc/apt/sources.list.d/vscode.list
-
-# GitHub CLI
-sudo mkdir -p -m 755 /etc/apt/keyrings
-wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null
-sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list
-
-# Mettre à jour les paquets une fois tous les dépôts ajoutés
+# Installation de MariaDB
+sudo apt install software-properties-common -y
+sudo apt-key adv --fetch-keys 'https://mariadb.org/mariadb_release_signing_key.asc'
+sudo add-apt-repository 'deb [arch=amd64] http://mariadb.mirror.liquidtelecom.com/repo/10.5/ubuntu $(lsb_release -cs) main'
 sudo apt update
+sudo apt install mariadb-server -y
+sudo mysql_secure_installation
 
-# Installer les applications
+# Configuration initiale de MariaDB
+sudo mysql <<EOF
+CREATE USER 'admin'@'localhost' IDENTIFIED BY 'Afpa1234';
+GRANT ALL PRIVILEGES ON *.* TO 'admin'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+EOF
+
+# Ajout des dépôts pour les applications et mise à jour des paquets
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /usr/share/keyrings/microsoft-edge-keyring.gpg >/dev/null
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/microsoft-edge-keyring.gpg] https://packages.microsoft.com/repos/edge stable main" | sudo tee /etc/apt/sources.list.d/microsoft-edge-dev.list
+
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /usr/share/keyrings/vscode-keyring.gpg >/dev/null
+echo "deb [arch=amd64,arm64,armhf signed-by=/usr/share/keyrings/vscode-keyring.gpg] https://packages.microsoft.com/repos/code stable main" | sudo tee /etc/apt/sources.list.d/vscode.list
+
+wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-keyring.gpg >/dev/null
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list
+
+sudo apt update
 sudo apt install google-chrome-stable microsoft-edge-stable code gh -y
 
-# Installation de Composer
+# Installation de Composer et Symfony CLI
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php composer-setup.php
 sudo mv composer.phar /usr/local/bin/composer
-rm -f composer-setup.php
+rm composer-setup.php
 
-# Installation de Symfony CLI
 echo 'deb [trusted=yes] https://repo.symfony.com/apt/ /' | sudo tee /etc/apt/sources.list.d/symfony-cli.list
+sudo apt update
 sudo apt install symfony-cli -y
 
-# Authentifier l'utilisateur sur GitHub
+# Authentification GitHub CLI
 gh auth login
 
 echo "Configuration de Git, GitHub CLI et autres outils terminée avec succès."
-
-# Configuration de MariaDB
-sudo mysql << EOF
-create user 'admin'@'localhost' identified by 'Afpa1234';
-grant all privileges on *.* to 'admin'@'localhost' with grant option;
-flush privileges;
-EOF
